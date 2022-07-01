165ed9dd35c1f17252b2f25691bb8227b9ccf766
kosuke-kurono <76575876+kosuke-kurono@users.noreply.github.com>
2022-07-01 15:48:10 +0900

    Add files via upload

---
+++ routes/web.php
@@ -0,0 +1,49 @@
+<?php
+
+use App\Http\Controllers\IndexController;
+use App\Http\Controllers\TodoListController;
+use GuzzleHttp\Middleware;
+use Illuminate\Foundation\Application;
+use Illuminate\Support\Facades\Route;
+use Inertia\Inertia;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return Inertia::render('Welcome', [
+        'canLogin' => Route::has('login'),
+        'canRegister' => Route::has('register'),
+        'laravelVersion' => Application::VERSION,
+        'phpVersion' => PHP_VERSION,
+    ]);
+});
+
+Route::get('/dashboard', function () {
+    return Inertia::render('Dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+// Route::get('/todo', [TodoListController::class,'index'])->name('todo.index')->middleware('auth');
+Route::resource('/todo',TodoListController::class)->names([
+    'index'=>'todo.index',
+    'create' => 'todo.create',
+    'store' => 'todo.store',
+    'destroy' => 'todo.destroy',
+    'edit' => 'todo.edit',
+    'update' => 'todo.update',
+
+    ])->middleware('auth');
+
+// テスト用
+Route::get('/index/{name?}', [IndexController::class, 'index']);
+
+
+require __DIR__.'/auth.php';
---
+++ storage/framework/views/f146962f5ed1cf54ce58eb04923988920a24430a.php
@@ -0,0 +1,2 @@
+<h2><?php echo e($name); ?></h2>
+<?php /**PATH C:\xampp\htdocs\laravel_app\resources\views/index.blade.php ENDPATH**/ ?>
